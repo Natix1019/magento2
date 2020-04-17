3d365945caa8f026a5d2ad2ed51a5053c8f089d2
Natix <chorpitaoles@gmail.com>
2020-04-17 19:04:24 +0300

    filter cards by date and status when loading page, added bootstrap, fixed bugs with triggering of some functions in unnecessary time

--- app/code/Mageplaza/HelloWorld/view/frontend/web/js/custom.js
+++ app/code/Mageplaza/HelloWorld/view/frontend/web/js/custom.js
@@ -18,7 +18,7 @@ define(['uiComponent', 'ko', 'jquery', 'moment'], function(Component, ko, $, mom
                         code : 'P25-GF3-24R',
                         dataStart: new Date(2019,8,1),
                         dataFinish: new Date(2020,2,1),
-                        status: 'used',
+                        status: 'unavailable',
                         id : '2'
                     },
                     {
@@ -34,7 +34,7 @@ define(['uiComponent', 'ko', 'jquery', 'moment'], function(Component, ko, $, mom
                         code : 'O5F-82E-WW3',
                         dataStart: new Date(2018,4,14),
                         dataFinish: new Date(2019,6,8),
-                        status : 'used',
+                        status : 'unavailable',
                         id : '4'
                     },
                     {
@@ -42,7 +42,7 @@ define(['uiComponent', 'ko', 'jquery', 'moment'], function(Component, ko, $, mom
                         code : '25J-F2K-G6G',
                         dataStart: new Date(2019,12,23),
                         dataFinish: new Date(2021,2,25),
-                        status : 'used',
+                        status : 'unavailable',
                         id : '5'
                     },
                     {
@@ -58,7 +58,7 @@ define(['uiComponent', 'ko', 'jquery', 'moment'], function(Component, ko, $, mom
                         code : 'FF2-K2K-GT5',
                         dataStart: new Date(2020,1,23),
                         dataFinish: new Date(2023,2,28),
-                        status : 'available',
+                        status : 'used',
                         id : '7'
                     },
                     {
@@ -132,14 +132,30 @@ define(['uiComponent', 'ko', 'jquery', 'moment'], function(Component, ko, $, mom
 
             return this;
         },
+        filterCart(){
+            this.carts.filter(cart=>{
+                let dateNaw = new Date();
+                if(dateNaw<=cart.dataFinish){
+                    // console.log(moment(cart.dataFinish).format('YYYY-MM-DD'));
+
+                }else {
+
+                    $('#id'+cart.id+'.front').css('display','none');
+                    $('#id'+cart.id+'.front-unavailable').css('display','block');
+                    // console.log(cart.id);
+                }
+            });
+        },
+
         validUser(event,data){
             let idCart=event.target.id;
             let id = document.querySelector('#'+idCart);
 
-            console.log(moment('2020/09/01').format('YYYY-MM-DD'));
+
+            // console.log(moment('2020/09/01').format('YYYY-MM-DD'));
 
             let nameUser = $('#'+idCart+'.textUser').val();
-            console.log(nameUser);
+            // console.log(nameUser);
             this.users.forEach(user =>{
                 if (user.name===nameUser) {
                 // let thisData = new Date();
@@ -148,7 +164,7 @@ define(['uiComponent', 'ko', 'jquery', 'moment'], function(Component, ko, $, mom
                             if(code === cart.code){
                                 let korectId = 'id'+cart.id;
                                 if(korectId == idCart){
-                                    console.log(cart.code+' '+cart.id);
+                                    // console.log(cart.code+' '+cart.id);
                                     this.transformCart(idCart);
                                 }
                         }
@@ -160,8 +176,8 @@ define(['uiComponent', 'ko', 'jquery', 'moment'], function(Component, ko, $, mom
         } ,
         transformCart: function(idCart){
             let id = document.querySelector('#'+idCart);
-            console.log(id);
-            let promise = new Promise((resolve)=>{});
+            // console.log(id);
+            // let promise = new Promise((resolve)=>{});
             // promise.then(result =>{for (let i=0;i<=90;i++){
             //     $('#'+idCart+'.front').css('transition-duration','4.5s');
             //     $('#'+idCart+'.front').css('transform','rotateY('+ i +'deg)');
@@ -174,39 +190,61 @@ define(['uiComponent', 'ko', 'jquery', 'moment'], function(Component, ko, $, mom
 
                     }
 
-    setTimeout(function () {
-            $('#'+idCart+'.front').css('display','none');
-            $('#'+idCart+'+.back').css('display','block');
-        setTimeout(function () {
-            for (let i = 90; i >= 0; i--) {
-                $('#' + idCart + '+.back').css('transition-duration', '4.5s');
-                $('#' + idCart + '+.back').css('transform', 'rotateY(' + i + 'deg)');
-
-            }
-        },1000);
-            },1500);
-        },
-        backCart : function (event) {
-            let idCart=event.target.id;
-            for (let i=0;i<=90;i++){
-                $('#'+idCart+'.back').css('transition-duration','1.5s');
-                $('#'+idCart+'.back').css('transform','rotateY('+ i +'deg)');
-
-            }
-
             setTimeout(function () {
-                $('#'+idCart+'.back').css('display','none');
-                $('#'+idCart+'.front').css('display','block');
-                console.log(idCart);
+                    $('#'+idCart+'.front').css('display','none');
+                    $('#'+idCart+'+.back').css('display','block');
                 setTimeout(function () {
                     for (let i = 90; i >= 0; i--) {
-                        $('#'+idCart+'.front').css('transition-duration', '4.5s');
-                        $('#'+idCart+'.front').css('transform', 'rotateY(' + i + 'deg)');
+                        $('#' + idCart + '+.back').css('transition-duration', '4.5s');
+                        $('#' + idCart + '+.back').css('transform', 'rotateY(' + i + 'deg)');
+
+                    }
+                },1000);
+            },1500);
+         },
+        backCart : function (event) {
+            let idCart=event.target.id;
+
+            let wait = new Promise(resolve => {
+                for (let i = 0; i <= 90; i++) {
+                    $('#' + idCart + '.back').css('transition-duration', '1.5s');
+                    $('#' + idCart + '.back').css('transform', 'rotateY(' + i + 'deg)');
+                    if (i === 90) {
+                        resolve(i);
+                    }
+                }
+            });
+
+            setTimeout(() => {
+                $('#'+idCart+'.back').css('display','none');
+                $('#'+idCart+'.front-used').css('display','block');
+                // console.log(idCart);
+                setTimeout(() => {
+                    for (let i = 90; i >= 0; i--) {
+                        $('#'+idCart+'.front-used').css('transition-duration', '4.5s');
+                        $('#'+idCart+'.front-used').css('transform', 'rotateY(' + i + 'deg)');
 
                     }
                 },1000);
 
             },1500);
+            this.carts.filter(cart=>{
+                cart.status = 'used';
+            })
+            // $('#'+idCart+'.back').promise().done(function () {
+            //     $('#'+idCart+'.back').css('display','none');
+            //     $('#'+idCart+'.front-used').css('display','block');
+            // });
+            // $('#'+idCart+'.front-used').promise().done(function () {
+            //     for (let i = 90; i >= 0; i--) {
+            //         $('#'+idCart+'.front-used').css('transition-duration', '4.5s');
+            //         $('#'+idCart+'.front-used').css('transform', 'rotateY(' + i + 'deg)');
+            //
+            //     }
+            // })
+
+
+
         },
 
 
