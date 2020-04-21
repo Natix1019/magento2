beeb7e0c163f0d6c40222b0d1fd96ff74f313455
Natix <chorpitaoles@gmail.com>
2020-04-21 17:15:28 +0300

    created name validation, accelerated animation, enters water with names, fixed style in mobile

--- app/code/Mageplaza/HelloWorld/view/frontend/web/js/custom.js
+++ app/code/Mageplaza/HelloWorld/view/frontend/web/js/custom.js
@@ -8,72 +8,90 @@ define(['uiComponent', 'ko', 'jquery', 'moment'], function(Component, ko, $, mom
                     {
                         name:'25% Promotion Cart',
                         code : 'PRO-82K-I34',
-                        dataStart: new Date(2020,1,5),
-                        dataFinish: new Date(2020,6,25),
+                        // dataStart: new Date(2020,1,5),
+                        dataStart:moment('2020/01/05').format('YYYY-MM-DD'),
+                        // dataFinish: new Date(2020,6,25),
+                        dataFinish:moment('2020/06/25').format('YYYY-MM-DD'),
                         status : 'available',
                         id : '1'
                     },
                     {
                         name:'45% Promotion Cart',
                         code : 'P25-GF3-24R',
-                        dataStart: new Date(2019,8,1),
-                        dataFinish: new Date(2020,2,1),
+                        dataStart:moment('2019/08/01').format('YYYY-MM-DD'),
+                            // new Date(2019,8,1),
+                        dataFinish:moment('2020/02/01').format('YYYY-MM-DD'),
+                            // new Date(2020,2,1),
                         status: 'unavailable',
                         id : '2'
                     },
                     {
                         name:'80% Promotion Cart',
                         code : 'KJ9-8SK-W4G',
-                        dataStart: new Date(2020,1,5),
-                        dataFinish: new Date(2020,2,21),
+                        dataStart: moment('2020/01/05').format('YYYY-MM-DD'),
+                            // new Date(2020,1,5),
+                        dataFinish: moment('2020/02/21').format('YYYY-MM-DD'),
+                            // new Date(2020,2,21),
                         status : 'available',
                         id : '3'
                     },
                     {
                         name:'12% Promotion Cart',
                         code : 'O5F-82E-WW3',
-                        dataStart: new Date(2018,4,14),
-                        dataFinish: new Date(2019,6,8),
+                        dataStart: moment('2018/04/14').format('YYYY-MM-DD'),
+                            // new Date(2018,4,14),
+                        dataFinish: moment('2019/06/08').format('YYYY-MM-DD'),
+                            // new Date(2019,6,8),
                         status : 'unavailable',
                         id : '4'
                     },
                     {
                         name:'5% Promotion Cart',
                         code : '25J-F2K-G6G',
-                        dataStart: new Date(2019,12,23),
-                        dataFinish: new Date(2021,2,25),
+                        dataStart: moment('2019/12/23').format('YYYY-MM-DD'),
+                            // new Date(2019,12,23),
+                        dataFinish: moment('2021/02/25').format('YYYY-MM-DD'),
+                            // new Date(2021,2,25),
                         status : 'unavailable',
                         id : '5'
                     },
                     {
                         name:'50% Promotion Cart',
                         code : '68F-22K-5G5',
-                        dataStart: new Date(2019,12,23),
-                        dataFinish: new Date(2021,11,12),
+                        dataStart: moment('2019/12/23').format('YYYY-MM-DD'),
+                            // new Date(2019,12,23),
+                        dataFinish: moment('2021/11/12').format('YYYY-MM-DD'),
+                            // new Date(2021,11,12),
                         status : 'available',
                         id : '6'
                     },
                     {
                         name:'75% Promotion Cart',
                         code : 'FF2-K2K-GT5',
-                        dataStart: new Date(2020,1,23),
-                        dataFinish: new Date(2023,2,28),
+                        dataStart: moment('2020/01/23').format('YYYY-MM-DD'),
+                            // new Date(2020,1,23),
+                        dataFinish: moment('2023/02/28').format('YYYY-MM-DD'),
+                            // new Date(2023,2,28),
                         status : 'used',
                         id : '7'
                     },
                     {
                         name:'95% Promotion Cart',
                         code : 'P4O-12K-I12',
-                        dataStart: new Date(2020,1,5),
-                        dataFinish: new Date(2020,2,25),
+                        dataStart: moment('2020/01/05').format('YYYY-MM-DD'),
+                            // new Date(2020,1,5),
+                        dataFinish: moment('2020/02/25').format('YYYY-MM-DD'),
+                            // new Date(2020,2,25),
                         status : 'available',
                         id : '8'
                     },
                     {
                         name:'80% Promotion Cart',
                         code : 'BRO-8GK-T40',
-                        dataStart: new Date(2019,8,5),
-                        dataFinish: new Date(2020,10,25),
+                        dataStart: moment('2019/08/05').format('YYYY-MM-DD'),
+                            // new Date(2019,8,5),
+                        dataFinish: moment('2020/10/25').format('YYYY-MM-DD'),
+                            // new Date(2020,10,25),
                         status : 'available',
                         id : '9'
                     }
@@ -84,46 +102,53 @@ define(['uiComponent', 'ko', 'jquery', 'moment'], function(Component, ko, $, mom
                     codes: [
                         'PRO-82K-I34',
                         'P25-GF3-24R'
-                    ]
+                    ],
+                    filterName:true
                 },
                 {
                     name:'Victor',
                     codes : [
                         'P25-GF3-24R',
                         'BRO-8GK-T40'
-                        ]
+                        ],
+                    filterName:true
                 },
                 {
                     name:'Harry',
                     codes : [
                         'KJ9-8SK-W4G'
-                        ]
+                        ],
+                    filterName:true
                 },
                 {
                     name:'Amelia',
                     codes : [
                         'O5F-82E-WW3'
-                        ]
+                        ],
+                    filterName:true
                 },
                 {
                     name:'Oscar',
                     codes : [
                         '25J-F2K-G6G',
                         'P25-GF3-24R'
-                        ]
+                        ],
+                    filterName:true
                 },
                 {
                     name:'Lily',
                     codes : [
                         '68F-22K-5G5'
-                        ]
+                        ],
+                    filterName:true
                 },
                 {
                     name:'James',
                     codes : [
                         'FF2-K2K-GT5',
                         'P4O-12K-I12'
-                        ]
+                        ],
+                    filterName:true
                 }
             ]
         },
@@ -135,27 +160,33 @@ define(['uiComponent', 'ko', 'jquery', 'moment'], function(Component, ko, $, mom
         filterCart(){
             this.carts.filter(cart=>{
                 let dateNaw = new Date();
-                if(dateNaw<=cart.dataFinish){
-                    // console.log(moment(cart.dataFinish).format('YYYY-MM-DD'));
-
+                if(dateNaw<=new Date(cart.dataFinish)){
+                    if (cart.status === 'used'){
+                        $('#id'+cart.id+'.front').css('display','none');
+                        $('#id'+cart.id+'.front-used').css('display','block');
+                        $('#id'+cart.id+'.front-used').css('transform','rotateY(0)');
+                    }
                 }else {
 
                     $('#id'+cart.id+'.front').css('display','none');
                     $('#id'+cart.id+'.front-unavailable').css('display','block');
-                    // console.log(cart.id);
                 }
             });
         },
 
-        validUser(event,data){
+        keyUpValid: function (data, event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                this.validUser(event);
+            }
+        },
+
+        validUser: function (event) {
             let idCart=event.target.id;
             let id = document.querySelector('#'+idCart);
-
-
-            // console.log(moment('2020/09/01').format('YYYY-MM-DD'));
+            let i=0;
 
             let nameUser = $('#'+idCart+'.textUser').val();
-            // console.log(nameUser);
             this.users.forEach(user =>{
                 if (user.name===nameUser) {
                 // let thisData = new Date();
@@ -164,26 +195,27 @@ define(['uiComponent', 'ko', 'jquery', 'moment'], function(Component, ko, $, mom
                             if(code === cart.code){
                                 let korectId = 'id'+cart.id;
                                 if(korectId == idCart){
-                                    // console.log(cart.code+' '+cart.id);
                                     this.transformCart(idCart);
                                 }
                         }
                     })
                 })
 
-            }
+            }else {
+                    i++;
+                    if (i===this.users.length){
+                            $('#'+idCart+'.errorName').css('display','block');
+                        setTimeout(()=>{
+                             $('#'+idCart+'.errorName').css('display','none');
+                        },5000);
+                    }
+
+
+                }
             })
         } ,
         transformCart: function(idCart){
             let id = document.querySelector('#'+idCart);
-            // console.log(id);
-            // let promise = new Promise((resolve)=>{});
-            // promise.then(result =>{for (let i=0;i<=90;i++){
-            //     $('#'+idCart+'.front').css('transition-duration','4.5s');
-            //     $('#'+idCart+'.front').css('transform','rotateY('+ i +'deg)');
-            //
-            // }});
-
                 for (let i=0;i<=90;i++){
                         $('#'+idCart+'.front').css('transition-duration','1.5s');
                         $('#'+idCart+'.front').css('transform','rotateY('+ i +'deg)');
@@ -199,8 +231,8 @@ define(['uiComponent', 'ko', 'jquery', 'moment'], function(Component, ko, $, mom
                         $('#' + idCart + '+.back').css('transform', 'rotateY(' + i + 'deg)');
 
                     }
-                },1000);
-            },1500);
+                },100);
+            },1100);
          },
         backCart : function (event) {
             let idCart=event.target.id;
@@ -218,33 +250,18 @@ define(['uiComponent', 'ko', 'jquery', 'moment'], function(Component, ko, $, mom
             setTimeout(() => {
                 $('#'+idCart+'.back').css('display','none');
                 $('#'+idCart+'.front-used').css('display','block');
-                // console.log(idCart);
                 setTimeout(() => {
                     for (let i = 90; i >= 0; i--) {
                         $('#'+idCart+'.front-used').css('transition-duration', '4.5s');
                         $('#'+idCart+'.front-used').css('transform', 'rotateY(' + i + 'deg)');
 
                     }
-                },1000);
+                },100);
 
-            },1500);
+            },1100);
             this.carts.filter(cart=>{
                 cart.status = 'used';
             })
-            // $('#'+idCart+'.back').promise().done(function () {
-            //     $('#'+idCart+'.back').css('display','none');
-            //     $('#'+idCart+'.front-used').css('display','block');
-            // });
-            // $('#'+idCart+'.front-used').promise().done(function () {
-            //     for (let i = 90; i >= 0; i--) {
-            //         $('#'+idCart+'.front-used').css('transition-duration', '4.5s');
-            //         $('#'+idCart+'.front-used').css('transform', 'rotateY(' + i + 'deg)');
-            //
-            //     }
-            // })
-
-
-
         },
 
 
